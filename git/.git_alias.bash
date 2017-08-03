fa60d0f6e6ab97ee337550b0857f13bdef95c832
Yongfu Li <liyongfu.sg@gmail.com>
2017-08-03 02:21:14 +0000

    add git alias commands

---
+++ git/.git_alias.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias gst="git status "
+alias gc="git commit -m "
+alias ga="git add "
+alias gr="git rm "
+alias gl="git log --graph --decorate --pretty=oneline --abbrev-commit --all --date=local"
+alias gcm="git checkout master"
+alias gco="git checkout"
+alias gph="git push"
+alias gb="git branch"
+alias gpl="git pull"
